75e641ba409c6ac6561881515dd59f80e70b825a
Tamarsh Abeysekera <abeysekt@oregonstate.edu>
2023-03-13 15:04:49 -0700

    fixed unused import & widget_test file.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:grateful/app.dart';
-import 'package:grateful/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
